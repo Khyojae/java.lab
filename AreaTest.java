d41a1683c06458df33ec3b78f0ca4e88f532bc58
Khyojae <khjae201@gmail.com>
2025-03-19 19:31:55 +0900

    0319

---
+++ AreaTest.java
@@ -0,0 +1,23 @@
+package hello;
+import java.util.Scanner;
+
+
+public class AreaTest {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int radius;
+		
+		System.out.print("반지름을 입력하시오: ");
+		radius = input.nextInt();
+		
+		final double PI =  3.141592;
+		double area = radius * radius * PI;
+		System.out.println("반지름이" + radius + "인 원의 면적은" + area + "입니다.");
+
+		
+	}
+
+}
+
+
